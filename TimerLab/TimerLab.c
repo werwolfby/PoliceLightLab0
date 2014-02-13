332ca250dc5fc5708dad0e972159df4852c3bf69
Alexander Puzynia <werwolf.by@gmail.com>
2014-02-13 23:12:30 +0300

    [*] First implementation using _delay_ms.

--- TimerLab/TimerLab.c
+++ TimerLab/TimerLab.c
@@ -7,10 +7,19 @@
 
 
 #include <avr/io.h>
+#include <util/delay.h>
 
 int main(void)
 {
+	PORTB |= _BV(PINB2);
+	PORTD |= _BV(PIND5);
+	
+	DDRD ^= _BV(PIND5);
+	
     while(1)
     {
+		DDRB ^= _BV(PINB2);
+		DDRD ^= _BV(PIND5);
+		_delay_ms(250);
     }
 }
