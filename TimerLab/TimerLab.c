1a2e9a4a4a038d617bf6b2f4d320f1a1d2bf4c3f
Alexander Puzynia <werwolf.by@gmail.com>
2014-02-13 23:52:38 +0300

    [*] Simplest PWM implementation.

--- TimerLab/TimerLab.c
+++ TimerLab/TimerLab.c
@@ -9,17 +9,30 @@
 #include <avr/io.h>
 #include <util/delay.h>
 
+#define FAST_PWM (_BV(WGM01) | _BV(WGM00))
+#define COM0A_SET (_BV(COM0A0) | _BV(COM0A1))
+#define COM0B_SET (_BV(COM0B0) | _BV(COM0B1))
+#define COM0B_CLEAR (_BV(COM0B1))
+#define PRESCALE_1 (_BV(CS00))
+#define PRESCALE_8 (_BV(CS01))
+#define PRESCALE_64 (_BV(CS00) | _BV(CS01))
+#define PRESCALE_256 (_BV(CS02))
+
 int main(void)
 {
-	PORTB |= _BV(PINB2);
-	PORTD |= _BV(PIND5);
+	DDRB |= _BV(DDB2);
+	DDRD |= _BV(DDD5);
 	
-	DDRD ^= _BV(PIND5);
+	TCCR0A |= FAST_PWM | COM0A_SET | COM0B_CLEAR;
+	TCCR0B |= PRESCALE_1;
+	
+	OCR0A = 10;
+	OCR0B = 10;
 	
     while(1)
     {
-		DDRB ^= _BV(PINB2);
-		DDRD ^= _BV(PIND5);
-		_delay_ms(250);
+		OCR0A += 10;
+		OCR0B += 10;
+		_delay_ms(20);
     }
 }
